6015c6fe6cb9927033fa20f46ae23f080ecd6349
natalia <nataly.levchenko@yahoo.com>
2023-04-07 21:56:35 -0700

    задание 4

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+        public string firstname;
+        public string lastname;
+        
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+}
